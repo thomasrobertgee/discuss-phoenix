6e2eb951b8d0300863d3d8932c864630d141532b
Thomas Gee <thomasrobertgee@gmail.com>
2017-03-09 17:17:43 +1100

    created form, onto submission

--- web/controllers/topic_controller.ex
+++ web/controllers/topic_controller.ex
@@ -1,7 +1,15 @@
 defmodule Discuss.TopicController do
   use Discuss.Web, :controller
 
+  alias Discuss.Topic
+
   def new(conn, params) do
-    
+    changeset = Topic.changeset(%Topic{}, %{})
+
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, params) do
+    IO.inspect(params)
   end
 end
