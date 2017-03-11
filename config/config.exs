ce4bdefcbc9b1a1580c1a220f0c405adb0d779d8
Thomas Gee <thomasrobertgee@gmail.com>
2017-03-11 23:17:37 +1100

    lots of progress made, basically set up OAuth with github, just now have to hook the button up and the rights of topics

--- config/config.exs
+++ config/config.exs
@@ -25,3 +25,12 @@ config :logger, :console,
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: {Ueberauth.Strategy.Github, []}
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
+  client_id: "28e8ce132e6fb5444da2",
+  client_secret: "f83c8f97b3d8eb9475040139bbd672ff9b14055d"
